ca822ab9f032778ac0694112ddfa31500804ae15
Jacob <mikhailxjacoborigin@onet.pl>
2020-08-22 13:38:10 +0200

    new func test

---
+++ dział 11 zad. 8 C/dział 11 zad. 8 C/dział 11 zad. 8 C.cpp
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+#include "pch.h"
+#include <iostream>
+#define SIZE 11
+char*zawiera_lan( char[],  char[],int);
+int main(void)
+{
+	char *pt2;
+	char s1[SIZE];
+	char s2[SIZE];
+	puts("Podaj dwa lancuchy:");
+	while (fgets(s1, SIZE-1, stdin) && fgets(s2, SIZE-1, stdin)) {
+		pt2 = zawiera_lan(s1, s2, SIZE);
+		printf("%c\n%p\n", pt2[0], pt2);
+	}
+	return 0;
+}
+char *zawiera_lan(char s1[], char s2[], int S) {
+	char*wsk=NULL;
+	int msc_w_s = 0,el_s1,el_s2=0;
+	for ( el_s1 = 0; el_s1 < (strlen(s1));el_s1++) {
+		if (s1[el_s1] == s2[el_s2]&&el_s2<(strlen(s2))) {
+			wsk = &s1[msc_w_s];
+			el_s2++;
+		}
+		else if(el_s2==strlen(s2)-1) {
+			el_s1 = S;
+		}
+		else {
+			wsk = NULL;
+			el_s2 = 0;
+			msc_w_s++;
+		}
+	}
+	return wsk;
+}
